04aa009acac9ac5eb566ecbc91332b64b2f4c40e
samba-98 <sambaiahgampa@gmail.com>
2024-11-11 21:56:50 +0530

    added yml file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   #credentials = "key.json"
-  project     = "terraform-demo-441306"
-  region      = "us-central1"
+  project = "terraform-demo-441306"
+  region  = "us-central1"
 }
